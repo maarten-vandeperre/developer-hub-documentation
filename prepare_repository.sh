1624654b1c9c24179376a623398d094b05fc10ae
Maarten Vandeperre @ Red Hat <mvandepe@redhat.com>
2024-07-15 09:58:28 +0200

    wrap up prepare_repository.sh

--- prepare_repository.sh
+++ prepare_repository.sh
@@ -9,6 +9,7 @@ fi
 
 # Get the base domain of the OpenShift cluster
 BASE_DOMAIN=$(oc get ingresses.config/cluster -o jsonpath='{.spec.domain}'; echo)
+BASE_DOMAIN="apps.cluster-test.dynamic.redhatworkshops.io"
 
 if [ -z "$BASE_DOMAIN" ]; then
     echo "Failed to retrieve the OpenShift cluster base domain."
@@ -16,10 +17,14 @@ if [ -z "$BASE_DOMAIN" ]; then
 fi
 
 # Define the placeholder to be replaced
-PLACEHOLDER="apps.cluster-b97l9.dynamic.redhatworkshops.io"
+PLACEHOLDER="apps.cluster-ldxcx.dynamic.redhatworkshops.io"
+ESCAPED_PLACEHOLDER=$(echo "$PLACEHOLDER" | sed 's/\./\\./g')
 
 # Find all files and replace the placeholder
-find / -type f -exec sed -i.bak "s|$PLACEHOLDER|$BASE_DOMAIN|g" {} +
+  #Linux
+  # find . -type f -exec sed -i.bak "s|$ESCAPED_PLACEHOLDER|$BASE_DOMAIN|g" {} +
+  # Mac
+  LC_CTYPE=C && LANG=C && find . -type f -exec sed -i.bak "s|$ESCAPED_PLACEHOLDER|$BASE_DOMAIN|g" {} +
 
 if [ $? -eq 0 ]; then
     echo "Successfully replaced the placeholder with the OpenShift cluster base domain in all files."
@@ -27,3 +32,7 @@ else
     echo "Failed to replace the placeholder in some files."
     exit 1
 fi
+
+# Delete all .bak files in the current directory and its subdirectories
+find . -type f -name "*.bak" -exec rm -f {} \;
+echo "All .bak files have been deleted."
