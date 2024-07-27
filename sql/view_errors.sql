f1a9507096b3f22e4132e662cb6dd1c30e5701b1
Maarten Vandeperre @ Red Hat <mvandepe@redhat.com>
2024-07-27 17:57:07 +0200

    add error sql query

--- sql/view_errors.sql
+++ sql/view_errors.sql
@@ -4,9 +4,10 @@ SELECT entity_id,
        errors,
        last_discovery_at AT TIME ZONE 'Europe/Brussels' AS last_discovery_at
 FROM refresh_state
--- WHERE last_discovery_at >= NOW() - INTERVAL '15 minutes'
-WHERE last_discovery_at AT TIME ZONE 'Europe/Brussels' >= '2024-07-27 12:19:28.943856'
--- WHERE entity_ref = 'system:default/maartens-wonderful-system'
+WHERE 1=1 -- dummy where clause to enable the "AND" clauses later on and play with commenting them out
+-- AND last_discovery_at >= NOW() - INTERVAL '15 minutes'
+AND last_discovery_at AT TIME ZONE 'Europe/Brussels' >= '2024-07-27 12:19:28.943856'
+-- AND entity_ref = 'system:default/maartens-wonderful-system'
 ORDER by last_discovery_at desc
 ;
 -- Last discovery: 2024-07-27 12:13:23.515733
