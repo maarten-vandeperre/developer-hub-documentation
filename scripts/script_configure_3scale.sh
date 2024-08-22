785ca9013dfec74b4114b65e6679217cd05cfd55
Maarten Vandeperre @ Red Hat <mvandepe@redhat.com>
2024-08-22 09:38:34 +0200

    fix 3scale automated installation

--- scripts/script_configure_3scale.sh
+++ scripts/script_configure_3scale.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+oc rsync \
+      ./sql $(oc get pod -n demo-project | grep mysql | grep Running | awk '{print $1}'):/tmp \
+      -n demo-project --include 3scale_data_dump.sql
+
 oc exec $(oc get pod -n demo-project | grep mysql | grep Running | awk '{print $1}') -n demo-project \
         -- mysql \
-        -u root \
+        -uroot \
         system \
-        < ./sql/3scale_data_dump.sql
+        < /tmp/sql/3scale_data_dump.sql
